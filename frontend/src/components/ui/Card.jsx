6220e01b5bba79f40d95dde6ae74e51b1086a3e8
Igor Henrique Lorenço <igorhenrique102@gmail.com>
2026-05-16 20:04:49 -0300

    feat: adiciona componente Card primitivo

---
+++ frontend/src/components/ui/Card.jsx
@@ -0,0 +1,5 @@
+import { cn } from '../../lib/utils'
+
+export function Card({ as: Component = 'div', className, ...props }) {
+    return <Component className={cn('flex flex-col', className)} {...props} />
+}
