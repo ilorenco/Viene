0719cb9c37cd15a3c488f6ecd5aaf06327c70217
Igor Henrique Lorenço <igorhenrique102@gmail.com>
2026-05-17 16:47:24 -0300

    feat: estende mock de eventos com endereço e horário

--- frontend/src/mocks/events.js
+++ frontend/src/mocks/events.js
@@ -1,14 +1,52 @@
+const defaultAddress = 'Rua Joinville, Zona Industrial Norte, Joinville - SC, 89239-220'
+const defaultDatetime = '13:00 - 15 Abr. 2025 > 19:00 - 15 Abr. 2025'
+
 export const mockEvents = [
-    { id: 1, title: 'Evento Pitch Inovaparq 2025' },
-    { id: 2, title: 'Hackathon Joinville Tech' },
-    { id: 3, title: 'Workshop UX Design' },
-    { id: 4, title: 'Meetup React Brasil' },
-    { id: 5, title: 'Conferência Startup Day' },
-    { id: 6, title: 'Bootcamp Cloud Computing' },
-    { id: 7, title: 'Festival de Inovação Catarinense' },
-    { id: 8, title: 'Demo Day Aceleradoras' },
-    { id: 9, title: 'Talk: Liderança em Tech' },
-    { id: 10, title: 'Encontro de Mulheres na Tecnologia' },
-    { id: 11, title: 'Imersão em Produto Digital' },
-    { id: 12, title: 'Open Day Universidades de SC' },
+    {
+        id: 1,
+        title: 'Evento Pitch Inovaparq 2025',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
+    {
+        id: 2,
+        title: 'Hackathon Joinville Tech',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
+    { id: 3, title: 'Workshop UX Design', address: defaultAddress, datetime: defaultDatetime },
+    { id: 4, title: 'Meetup React Brasil', address: defaultAddress, datetime: defaultDatetime },
+    { id: 5, title: 'Conferência Startup Day', address: defaultAddress, datetime: defaultDatetime },
+    {
+        id: 6,
+        title: 'Bootcamp Cloud Computing',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
+    {
+        id: 7,
+        title: 'Festival de Inovação Catarinense',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
+    { id: 8, title: 'Demo Day Aceleradoras', address: defaultAddress, datetime: defaultDatetime },
+    { id: 9, title: 'Talk: Liderança em Tech', address: defaultAddress, datetime: defaultDatetime },
+    {
+        id: 10,
+        title: 'Encontro de Mulheres na Tecnologia',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
+    {
+        id: 11,
+        title: 'Imersão em Produto Digital',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
+    {
+        id: 12,
+        title: 'Open Day Universidades de SC',
+        address: defaultAddress,
+        datetime: defaultDatetime,
+    },
 ]
