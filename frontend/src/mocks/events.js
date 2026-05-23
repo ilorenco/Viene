7d7b52ef32f13bc99830e99913615cff65f71f9f
Igor Henrique Lorenço <igorhenrique102@gmail.com>
2026-05-22 23:01:12 -0300

    feat: estende mock de eventos com descrição e políticas

--- frontend/src/mocks/events.js
+++ frontend/src/mocks/events.js
@@ -1,52 +1,28 @@
 const defaultAddress = 'Rua Joinville, Zona Industrial Norte, Joinville - SC, 89239-220'
 const defaultDatetime = '13:00 - 15 Abr. 2025 > 19:00 - 15 Abr. 2025'
+const defaultDescription =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae tellus nec metus feugiat varius. Morbi at nisi a risus lacinia hendrerit. Vivamus ac sapien sed enim finibus eleifend. Sed at justo nec ipsum lobortis feugiat. Nulla facilisi.'
+const defaultPolicies =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae tellus nec metus feugiat varius. Morbi at nisi a risus lacinia hendrerit. Vivamus ac sapien sed enim finibus eleifend. Sed at justo nec ipsum lobortis feugiat. Nulla facilisi.'
+
+const defaults = {
+    address: defaultAddress,
+    datetime: defaultDatetime,
+    description: defaultDescription,
+    policies: defaultPolicies,
+}
 
 export const mockEvents = [
-    {
-        id: 1,
-        title: 'Evento Pitch Inovaparq 2025',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
-    {
-        id: 2,
-        title: 'Hackathon Joinville Tech',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
-    { id: 3, title: 'Workshop UX Design', address: defaultAddress, datetime: defaultDatetime },
-    { id: 4, title: 'Meetup React Brasil', address: defaultAddress, datetime: defaultDatetime },
-    { id: 5, title: 'Conferência Startup Day', address: defaultAddress, datetime: defaultDatetime },
-    {
-        id: 6,
-        title: 'Bootcamp Cloud Computing',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
-    {
-        id: 7,
-        title: 'Festival de Inovação Catarinense',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
-    { id: 8, title: 'Demo Day Aceleradoras', address: defaultAddress, datetime: defaultDatetime },
-    { id: 9, title: 'Talk: Liderança em Tech', address: defaultAddress, datetime: defaultDatetime },
-    {
-        id: 10,
-        title: 'Encontro de Mulheres na Tecnologia',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
-    {
-        id: 11,
-        title: 'Imersão em Produto Digital',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
-    {
-        id: 12,
-        title: 'Open Day Universidades de SC',
-        address: defaultAddress,
-        datetime: defaultDatetime,
-    },
+    { id: 1, title: 'Evento Pitch Inovaparq 2025', ...defaults },
+    { id: 2, title: 'Hackathon Joinville Tech', ...defaults },
+    { id: 3, title: 'Workshop UX Design', ...defaults },
+    { id: 4, title: 'Meetup React Brasil', ...defaults },
+    { id: 5, title: 'Conferência Startup Day', ...defaults },
+    { id: 6, title: 'Bootcamp Cloud Computing', ...defaults },
+    { id: 7, title: 'Festival de Inovação Catarinense', ...defaults },
+    { id: 8, title: 'Demo Day Aceleradoras', ...defaults },
+    { id: 9, title: 'Talk: Liderança em Tech', ...defaults },
+    { id: 10, title: 'Encontro de Mulheres na Tecnologia', ...defaults },
+    { id: 11, title: 'Imersão em Produto Digital', ...defaults },
+    { id: 12, title: 'Open Day Universidades de SC', ...defaults },
 ]
