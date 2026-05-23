e8090df3ebe046794b8b56d961ba319be15c18b7
Igor Henrique Lorenço <igorhenrique102@gmail.com>
2026-05-22 21:08:50 -0300

    feat: adiciona mock de atores

---
+++ frontend/src/mocks/actors.js
@@ -0,0 +1,13 @@
+const defaultDescription =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt.'
+
+export const mockActors = [
+    { id: 1, name: 'Inovaparq', description: defaultDescription },
+    { id: 2, name: 'Perini Business Park', description: defaultDescription },
+    { id: 3, name: 'Tecnoparq', description: defaultDescription },
+    { id: 4, name: 'Sapiens Parque', description: defaultDescription },
+    { id: 5, name: 'Cetiq', description: defaultDescription },
+    { id: 6, name: 'Acate', description: defaultDescription },
+    { id: 7, name: 'Softville', description: defaultDescription },
+    { id: 8, name: 'Univille', description: defaultDescription },
+]
