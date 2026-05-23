cd425e2ef908c25558e65e8902f9c5c33b78feb1
Igor Henrique Lorenço <igorhenrique102@gmail.com>
2026-05-23 12:52:17 -0300

    refactor: simplifica url de favoritos para /favorites

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
                 element: <Home />,
             },
             {
-                path: '/my-favorites',
+                path: '/favorites',
                 element: <MyFavorites />,
             },
             {
